0d10b444f9fee64efd6f4f5790943995c4f4f3dd
Gerard van Helden <gerard@zicht.nl>
2016-02-22 18:24:28 +0100

    adding comments

--- maze.hs
+++ maze.hs
@@ -1,29 +1,59 @@
 import Codec.Picture
 
-data T = Wall | Path deriving (Eq, Show)
-data Dir = North | East | South | West deriving Show
-data Rot = CW | CCW | Turn | None
-data Pos = Pos Int Int deriving (Eq, Show)
-data State = State Pos Dir deriving Show
-data Maze = Maze [[T]]
+data Tile 
+    = Wall 
+    | Path 
+    deriving (Eq, Show)
 
+data Dir 
+    = North 
+    | East 
+    | South 
+    | West 
+    deriving Show
+
+data Rot 
+    = CW            -- Rotate clockwise
+    | CCW           -- Rotate counter clockwise
+    | Turn          -- Turn around
+    | None          -- Don't rotate
+
+data Pos 
+    = Pos Int Int           -- A position is composed of two Int coordinates
+    deriving (Eq, Show)
+
+data State 
+    = State Pos Dir 
+    deriving Show
+
+data Maze               
+    = Maze [[Tile]]     -- A maze consists of rows of columns of Tiles (i.e. two-dimensional list)
+
+-- Render a printable version of the Tile type
+printable :: Tile -> String
 printable Wall = "#"
 printable Path = " "
 
-at :: Maze -> Pos -> T
+-- Get the type of tile at the given position from the given Maze. Is not index-safe
+at :: Maze -> Pos -> Tile
 at (Maze maze) (Pos x y) = maze !! x !! y
 
+-- Test if the given position within the maze is the edge of the entire maze
+isEdge :: Maze -> Pos -> Bool
 isEdge (Maze maze) (Pos x y) = x == 0 || y == 0 || x == length maze - 1 || y == length (maze !! x) -1 
 
+-- Test if the given position is existent within the maze
 valid :: Maze -> Pos -> Bool
 valid (Maze maze) (Pos x y) = x < (length maze) && y < (length $ maze !! x)
 
+-- Translate a position one step in the given direction
 translate :: Pos -> Dir -> Pos
 translate (Pos x y) West = Pos (x - 1) y
 translate (Pos x y) East = Pos (x + 1) y
 translate (Pos x y) North = Pos x (y - 1)
 translate (Pos x y) South = Pos x (y + 1)
 
+-- Rotate a direction by a given rotation
 rotate :: Dir -> Rot -> Dir
 rotate North CW = East
 rotate East CW = South
@@ -33,9 +63,14 @@ rotate d CCW = rotate (rotate (rotate d CW) CW) CW
 rotate d Turn = rotate (rotate d CW) CW
 rotate d None = d
 
+-- Returns the preferred list of rotations. This does a "Wall follower" algorithm
+-- by preferring to rotate right (CW), then proceeding ahead (None), then left (CCW), 
+-- and ultimately turn back
 rotations :: Dir -> [Dir]
 rotations d = map (rotate d) [CW, None, CCW, Turn]
 
+-- Find the next position given the current maze, position and looking direction, iterating over the passed 
+-- directions, yielding a new state or Nothing if no move can be calculated
 nextPos :: Maze -> Pos -> Dir -> [Dir] -> Maybe State 
 nextPos maze pos dir [] = Nothing
 nextPos maze pos dir l = 
@@ -45,6 +80,7 @@ nextPos maze pos dir l =
             then Just (State n t)
             else nextPos maze pos dir $ tail l
 
+-- Walk the maze by 
 walk :: Maze -> Pos -> Dir -> [Pos] -> IO()
 walk maze pos dir visited = 
     let n' = nextPos maze pos dir (rotations dir) in
